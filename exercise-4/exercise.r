645a0230b9a176d3abf7b358dfbbf771b45f6737
吴限 <wuxian@D-173-250-147-203.dhcp4.washington.edu>
2017-04-26 14:29:13 -0700

    changes

--- exercise-4/exercise.r
+++ exercise-4/exercise.r
@@ -4,33 +4,37 @@ library(httr)
 
 # Make a variable base.url that has the same base url from the omdb documentation.
 # (Hint: visit https://www.omdbapi.com/ to find the base url)
-
+base.url <- "http://www.omdbapi.com/?"
 
 # Make a variable called movie that has the name of your favorite movie
-
+movie <- "Harry Potter"
 
 # Make a variable called "query.parameters" that holds a list of the parameters
 # to pass to the API. View the OMDb documentation to see which parameters
 # are available.
-
+query.parameters <- list(t = movie, r = "json")
 
 # Make a variable called request that is a string of a request URL made up of the base URL
 # and the parameters string
-
+request <- GET(base.url, query = query.parameters)
+body <- content(request, "text")
 
 # Use fromJSON to retrieve JSON data from the omdb api using your request.
 # Store the result in a variable called movie.data
-
+movie.data <- fromJSON(body)
 
 # Make movie_data into a data frame using as.data.frame
-
+movie.dataframe <- as.data.frame(movie.data)
 
 # Write a function called Director that accepts a data frame of movie info and returns
 # A vector of strings that states a movie and the director of said movie.
-
+Director <- function(df) {
+  result <- paste(df$Title, "by", df$Director)
+  return(result)
+}
 
 # Call Director with your favorite movie, and assign it to the variable movie.director
-
+movie.director <- Director(movie.dataframe)
 
 
 # Bonus #
@@ -39,13 +43,18 @@ library(httr)
 # movies. 
 
 # Start by making a vecotr of movies and save it to the variable movie.list
-
+movie.list <- c("Logan", "The Lego Batman Movie", "A Dog's Purpose")
 # Write a function to make your API request and process the data
-
+make.API.request <- function(movie) {
+  query.parameters <- list(t = movie, r = "json")
   # Make a variable called request that is a string of a request URL made up of the base URL
   # and the parameters string
-
+  request <-GET(base.url, query = query.parameters)
+  body <- content(request, "text")
+  movie.data <- as.data.frame(fromJSON(body))
+  return(movie.data)
+}
 # For every entry in the vector api.request, APPLY the function fromJSON to make a list of lists
 # one entry for each request and assign this to a variable called data. 
 # (Hint: ?lapply. It's similar a 'for' loop but better!)
-
+movies.data <- lapply(movie.list, make.API.request)
